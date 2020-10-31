70e2c611c5e2d30c2141b2cfbc21b38b934267e2
Rashomon <mohammad18018@mail.unpad.ac.id>
2020-10-31 20:33:21 +0700

    deploy

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoI v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)"/>
+        </div>
+    </div>
+</template>
+<script>
+import TodoI from './TodoItem.vue';
+
+export default {
+    name:"Todos",
+    components: {
+        TodoI
+    },
+    props: ["todos"]
+}
+</script>
+<style scoped>
+
+</style>
